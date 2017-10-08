951df4424b3f843e27a30769fef8658cea2c6764
HP <akhilbidhuri@gmail.com>
2017-10-08 07:58:45 +0530

    added geo

--- geo.py
+++ geo.py
@@ -1,26 +1,39 @@
 from pymongo  import MongoClient
 import json
+import requests
+from requests.auth import HTTPDigestAuth
+key="voZenGjtOCQ7cLYJck6dAPqeC383khGx"
+secret="WwBI0lWjTm21vzhX"
 
-url='https://api.pitneybowes.com/location-intelligence/geocode-service/v1/transient/premium/geocode'
-
-def cordinates(del_add):
+def cordinates():
     client = MongoClient('mongodb://bidhuri:jaisiaram1@ds113445.mlab.com:13445/pbcall')
     db = client.get_default_database()
+    r = requests.get('https://api.pitneybowes.com/oauth')
+    r.auth = (key, secret)
+    
     
     op=db.delivery.find({})
+    lg=[]
+    headers = {'content-type': 'json'}
     for q in op:
         
         add=q['address']
         country=q['country']
         #print (add+country)
-        res=url
-        res1=url_route
-        lg=[]
-        if res1['distance']<7000 or res1['time']<25 :
-           lg.append(q['Phone']) 
-        return lg
+        parameters = {"country": country, "mainAddress": add}
+        
+        response = requests.request('POST',url='https://api.pitneybowes.com/location-intelligence/geocode-service/v1/transient/premium/geocode',data=parameters,auth=(key,secret))
+        print (response)
+        #res1=url_route
+        
+        #if res1['distance']<7000 or res1['time']<25 :
+         #  lg.append(q['Phone']) 
+    #return lg
     
 
 
 if __name__=="__main__":
     cordinates()
+    
+    
+    
