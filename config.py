32843c3bbe5c79945671ca806ddc559de3a26bc6
RUthvik Vijayakumar <coderuth@outlook.com>
2017-10-09 03:38:38 +0530

    added things

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 MONGO_URL = 'mongodb://bidhuri:jaisiaram1@ds113445.mlab.com:13445/pbcall'
-account_sid = "AC75d65769ec86e0555ae48bc2b245a1b3"
-auth_token = "d886780ec3cc9794b69ecfc051e03817"
-TWILIO_NUMBER = "+12064623728"
-GEOCODE_API_URL = 'https://api.pitneybowes.com/location-intelligence/geocode-service/v1/transient/premium/geocode'
-ROUTE_API_URL = 'https://api.pitneybowes.com/location-intelligence/georoute/v1/route/byaddress'
-TOKEN_URL = 'https://api.pitneybowes.com/oauth/token'
 
-PB_KEY = "Cn4GXLgGDgAXpRHM7YzbTkHGYKS6UEbC"
+account_sid   = "AC75d65769ec86e0555ae48bc2b245a1b3"
+auth_token    = "d886780ec3cc9794b69ecfc051e03817"
+TWILIO_NUMBER = "+12064623728"
+
+GEOCODE_API_URL = 'https://api.pitneybowes.com/location-intelligence/geocode-service/v1/transient/premium/geocode'
+ROUTE_API_URL   = 'https://api.pitneybowes.com/location-intelligence/georoute/v1/route/byaddress'
+TOKEN_URL       = 'https://api.pitneybowes.com/oauth/token'
+
+PB_KEY    = "Cn4GXLgGDgAXpRHM7YzbTkHGYKS6UEbC"
 PB_SECRET = "QybpKXkkX495Nors"
